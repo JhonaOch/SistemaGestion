65a752b2dee7a744a9bd36f6ebce71d04abfa84e
JonnathanOchoa <UPSJhona@users.noreply.github.com>
2019-05-02 00:19:15 -0500

    Correciones login

--- public/controladores/login.php
+++ public/controladores/login.php
@@ -6,21 +6,15 @@
  $usuario = isset($_POST["correo"]) ? trim($_POST["correo"]) : null;
  $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null;
  $sql = "SELECT * FROM usuario WHERE usu_correo = '$usuario' and usu_password =MD5('$contrasena')";
-
-
-
  $result = $conn->query($sql);
-
  
-                if(isset($_SESSION['nombre'])){
-                    echo"<p>Administrador: <span>".$_SESSION['nombre']."</span></p>";
-                  
-                    header("Location: ../../admin/vista/usuario/index.php");
-                }else{
-                    echo"<a href='login.php'>Iniciar Sesion</a>";
-                }
+ if ($result->num_rows > 0) {
+    
+ $_SESSION['isLogged'] = TRUE;
+ header("Location: ../../admin/vista/usuario/index.php");
+ } else {
+ header("Location: ../vista/login.html");
+ }
 
  $conn->close();
-
-
 ?>
